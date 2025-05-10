d6a498c2f09b5bd181849845af66b4fb3834f526
Kamran Gayibov <qayibovkamran@gmail.com>
2025-05-10 15:36:21 +0400

    Update docker-compose configuration and database connection logic - Changed database service to use PostgreSQL with health checks and persistent storage. - Updated connection string to disable SSL mode. - Removed unused query logic from database connection function.

--- db/database.go
+++ db/database.go
@@ -15,7 +15,7 @@ var DB *sql.DB
 // Connect initializes the database connection
 func Connect() error {
 	connStr := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		config.DBHost, config.DBPort, config.DBUser, config.DBPassword, config.DBName,
 	)
 
@@ -32,31 +32,6 @@ func Connect() error {
 		return fmt.Errorf("failed to ping the database: %v", err)
 	}
 	logger.Logger.Info("Database connected successfully")
-
-	rows, err := DB.Query("SELECT * FROM playing_with_neon")
-	if err != nil {
-		logger.Logger.Error("Failed to retrieve query", slog.String("error", err.Error()))
-		return fmt.Errorf("failed to retrieve query: %v", err)
-	}
-	defer rows.Close()
-
-	// Print rows
-	for rows.Next() {
-		var col1 int    // Replace with your actual column types
-		var col2 string // Replace with your actual column types
-		var col3 float64
-		err := rows.Scan(&col1, &col2, &col3) // Adjust based on your column count and types
-		if err != nil {
-			logger.Logger.Error("Failed to scan row", slog.String("error", err.Error()))
-			return fmt.Errorf("failed to scan row: %v", err)
-		}
-		logger.Logger.Debug("Row fetched", slog.Int("col1", col1), slog.String("col2", col2), slog.Float64("col3", col3))
-	}
-
-	if err := rows.Err(); err != nil {
-		logger.Logger.Error("Error during row iteration", slog.String("error", err.Error()))
-		return fmt.Errorf("error during row iteration: %v", err)
-	}
 
 	return nil
 }
